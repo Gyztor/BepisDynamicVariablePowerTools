097723b45601e01b8498c1776d9f6ac8743b491f
BowenArrows2K <bowen.quaid@gmail.com>
2026-07-07 18:03:23 -0400

    Add more settings

--- BowenArrowsMod/BowenArrowsMod.cs
+++ BowenArrowsMod/BowenArrowsMod.cs
@@ -12,15 +12,21 @@ public class BowenArrowsMod : BasePlugin
 {
     internal static new ManualLogSource Log = null!;
 
-    internal static ConfigEntry<bool> Enabled;
+    internal static ConfigEntry<bool> Mod_Enabled;
     internal static ConfigEntry<bool> ChangeProtoFluxStringInputs;
+    internal static ConfigEntry<bool> DebugInfo_Enabled;
+    internal static ConfigEntry<bool> DebugInfo_LinkedVars;
+    internal static ConfigEntry<bool> DebugInfo_CompHierarchy;
 
     public override void Load()
     {
         Log = base.Log;
         Log.LogInfo($"Plugin {PluginMetadata.GUID} is loaded!");
         HarmonyInstance.PatchAll();
-        Enabled = Config.Bind("Toggles", "Enabled", true);
-        ChangeProtoFluxStringInputs = Config.Bind("Toggles", "Change ProtoFlux String Inputs", false);
+        Mod_Enabled = Config.Bind("General", "Enabled", true);
+        ChangeProtoFluxStringInputs = Config.Bind("General", "Change ProtoFlux String Inputs on Rename", false);
+        DebugInfo_Enabled = Config.Bind("Debug Info", "Enabled", true);
+        DebugInfo_LinkedVars = Config.Bind("Debug Info", "Output Linked Variables", true);
+        DebugInfo_CompHierarchy = Config.Bind("Debug Info", "Output Component Hierarchy", true);
     }
 }
